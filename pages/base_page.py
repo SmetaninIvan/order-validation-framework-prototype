272517123c0ef33bdedb149ed524d14e88eac910
SmetaninIvan <ismetanin919@gmail.com>
2026-05-07 14:01:13 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,6 @@
 class BasePage:
-    def __init__(self, driver, db = None):
+    def __init__(self, driver):
         self.driver = driver
-        self.db = db
 
     def find(self, locator):
         return self.driver.find_element(*locator)
